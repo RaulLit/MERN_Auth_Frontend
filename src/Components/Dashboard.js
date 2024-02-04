b82bb6e0587a4503cd9846fc76a3e85077b55547
Aditya Raul <raul.aditya912@gmail.com>
2024-02-04 23:56:11 +0530

    changed ordering method of drag n drop

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -9,6 +9,7 @@ export const Dashboard = () => {
     { id: 3, title: "map tasks" },
     { id: 4, title: "edit button" },
   ]);
+  const [editMode, setEditMode] = useState(false);
 
   const dragTask = useRef(0);
   const draggedOverTask = useRef(0);
@@ -16,8 +17,8 @@ export const Dashboard = () => {
   function handleSort() {
     const tasksClone = [...tasks];
     const temp = tasksClone[dragTask.current];
-    tasksClone[dragTask.current] = tasksClone[draggedOverTask.current];
-    tasksClone[draggedOverTask.current] = temp;
+    tasksClone.splice(dragTask.current, 1);
+    tasksClone.splice(draggedOverTask.current, 0, temp);
     setTasks(tasksClone);
   }
 
@@ -25,12 +26,14 @@ export const Dashboard = () => {
     <div className="dashboardpage">
       <h1>Welcome back {user}</h1>
       <h2>This is a Dashboard</h2>
+      <button onClick={() => setEditMode(!editMode)}>{editMode ? "save" : "edit"}</button>
       <div
         style={{
           width: "50vw",
           height: "50vh",
           border: "1px solid black",
           display: "flex",
+          flexWrap: "wrap",
         }}
       >
         {tasks &&
@@ -38,8 +41,12 @@ export const Dashboard = () => {
             <div
               key={task.id}
               className="box"
-              style={{ background: `hsl(1${task.id + 3}5, 50%, 50%)` }}
-              draggable
+              style={{
+                background: `hsl(1${task.id + 3}5, 50%, 50%)`,
+                cursor: editMode ? "grab" : "default",
+                boxShadow: editMode ? "0 0 0.5rem rgba(0,0,0,0.5)" : "none",
+              }}
+              draggable={editMode}
               onDragStart={() => (dragTask.current = index)}
               onDragEnter={() => (draggedOverTask.current = index)}
               onDragEnd={handleSort}
